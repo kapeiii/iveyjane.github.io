db89c7ec6d257020b0abd97a335d14299cc40b1f
kapeiii <138109641+kapeiii@users.noreply.github.com>
2023-08-18 14:33:01 +0800

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`August 18 ${currentYear} 00:00:00`);
+const newYearTime = new Date(`August 18 ${2024} 00:00:00`);
 
 // Update countdown time
 function updateCountdown() {
